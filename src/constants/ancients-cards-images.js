7cc07e85a74e9ec5a4ce8ce1b21bf7b1f62e8af1
4k1r1n <arinakire@gmail.com>
2023-08-12 04:27:29 +0300

    refactor: distribution of constants across different files

---
+++ src/constants/ancients-cards-images.js
@@ -0,0 +1,13 @@
+import azathoth from '../assets/images/ancients/Azathoth.jpg';
+import cthulhu from '../assets/images/ancients/Cthulthu.jpg';
+import iogSothoth from '../assets/images/ancients/IogSothoth.jpg';
+import shubNiggurath from '../assets/images/ancients/ShubNiggurath.jpg';
+
+const ancientsCardsImages = {
+  azathoth,
+  cthulhu,
+  iogSothoth,
+  shubNiggurath,
+};
+
+export default ancientsCardsImages;
---
+++ src/constants/backgrounds.js
@@ -0,0 +1,13 @@
+import azathothBackground from '../assets/images/backgrounds/Azathoth.webp';
+import cthulhuBackground from '../assets/images/backgrounds/Cthulhu.webp';
+import iogSothothBackground from '../assets/images/backgrounds/IogSothoth.webp';
+import shubNiggurathBackground from '../assets/images/backgrounds/ShubNiggurath.webp';
+
+const backgrounds = {
+  azathoth: azathothBackground,
+  cthulhu: cthulhuBackground,
+  iogSothoth: iogSothothBackground,
+  shubNiggurath: shubNiggurathBackground,
+};
+
+export default backgrounds;
