ddd4f67c2cc22db5fe9f7be4036f60bb4ee547a6
4k1r1n <arinakire@gmail.com>
2023-08-12 16:04:25 +0300

    feat: add ancient-card component

---
+++ src/components/ancient-card/ancient-card.js
@@ -0,0 +1,27 @@
+import './ancient-card.css';
+import BaseComponent from '../../utils/base-component';
+
+class AncientCard extends BaseComponent {
+  constructor(ancient, index) {
+    super({
+      tagName: 'li',
+      className: 'ancient-card',
+    });
+    this.ancientImage = new BaseComponent({
+      tagName: 'img',
+      className: 'ancient-card__image',
+    });
+    this.ancient = ancient;
+    this.index = index;
+    this.ancientImage.setAttribute('src', this.ancient.cardFace);
+    this.ancientImage.setAttribute('alt', `${this.ancient.name}`);
+    this.setAttribute('data-id', `${this.index}`);
+    this.appendToDom(this.ancientImage.node);
+
+    this.node.addEventListener('click', () => {
+      this.addClass('active');
+    });
+  }
+}
+
+export default AncientCard;
