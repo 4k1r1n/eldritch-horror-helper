a6a435205eb9a0d90fea2e0b3f5209e7c398744e
4k1r1n <arinakire@gmail.com>
2023-08-18 20:40:02 +0300

    fix: click on a card from the deck

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,6 +3,8 @@ import BaseComponent from '../../utils/base-component';
 import cardBackground from '../../assets/images/mythic-cards/back.jpg';
 
 class Card extends BaseComponent {
+  static prevCard = null;
+
   constructor(cardInfo, onCardClick) {
     super({
       className: 'deck__card card',
@@ -15,18 +17,35 @@ class Card extends BaseComponent {
       tagName: 'img',
       className: 'card__face card__face_back',
     });
+    this.cardInfo = cardInfo;
+    this.onCardClick = onCardClick;
     this.cardFaceFront.node.src = cardBackground;
     this.cardFaceBack.node.src = cardInfo.cardFace;
-    this.node.addEventListener('transitionend', () => {
-      onCardClick(cardInfo);
-    });
-    this.node.addEventListener('click', () => {
-      this.addClass('is-flipped');
-    });
+    this.isAnimating = false;
+    this.addListener();
   }
 
   render() {
     this.appendToDom(this.cardFaceBack.node, this.cardFaceFront.node);
+  }
+
+  addListener() {
+    this.clickHandler = () => {
+      if (Card.prevCard && Card.prevCard.isAnimating) {
+        return;
+      }
+      this.addClass('is-flipped');
+      this.node.addEventListener('transitionend', this.handleTransitionEnd);
+      Card.prevCard = this;
+      this.isAnimating = true;
+    };
+    this.handleTransitionEnd = () => {
+      this.onCardClick(this.cardInfo);
+      this.node.removeEventListener('transitionend', this.handleTransitionEnd);
+      this.node.removeEventListener('click', this.clickHandler);
+      this.isAnimating = false;
+    };
+    this.node.addEventListener('click', this.clickHandler);
   }
 }
 
